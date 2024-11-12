6febac97cfd615d4a4ddae0022d154606e9f0454
bchen395 <bchen395@gmail.com>
2024-11-12 13:29:31 -0500

    Replace button with slider, add 7 images

--- myscripts.js
+++ myscripts.js
@@ -1,27 +1,52 @@
 const paintings = document.querySelectorAll("#painting")
-const change = document.querySelector("button");
-let num = 1;
-change.addEventListener("click", () => {
-    if (num===1){
-        num = 2;
-    }
-    else if (num===2) {
-        num = 1;
-    }
-    console.log(num);
-    switch(num){
-        case 1:
-            paintings.forEach((item) => {
-                item.src="images/untitled-1969.Kate.png";
-            });
-            break;
-        case 2:
+let slider = document.getElementById("myRange");
+
+slider.oninput = function() {
+    switch(slider.value){
+        case "1":
             paintings.forEach((item) => {
                 item.src="images/untitled-1969.SFMOMA.jpg";
             });
             break;
+        case "2":
+            paintings.forEach((item) => {
+                item.src="images/untitled-1969.Kate.png";
+                console.log(item.src);
+            });
+            break;
+        case "3":
+            paintings.forEach((item) => {
+                item.src="images/natgallery1968.png";
+                console.log(item.src);
+            });
+            break;
+        case "4":
+            paintings.forEach((item) => {
+                item.src="images/foundation1968.jpg";
+                console.log(item.src);
+            });
+            break;
+        case "5":
+            paintings.forEach((item) => {
+                item.src="images/estate1969.png";
+                console.log(item.src);
+            });
+            break;
+        case "6":
+            paintings.forEach((item) => {
+                item.src="images/chris1969.jpg";
+                console.log(item.src);
+            });
+            break;
+        case "7":
+            paintings.forEach((item) => {
+                item.src="images/blackform1964.jpg";
+                console.log(item.src);
+            });
+            break;
+            
     }
-});
+};
 
 
 // delete html images, add/delete
